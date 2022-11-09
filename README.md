d76892c06194a7e923e7aec6261e34eca3141419
sjbatista <salomaojunio@hotmail.com>
2022-11-09 11:34:21 -0300

    :rocket: new updates!

---
+++ README.md
@@ -0,0 +1,32 @@
+# ⚽ World Cup match schedule ⚽
+
+</br>
+
+<h1>
+
+Project created during the NLW-Cup event to display a schedule of group G games.
+
+</br>
+
+
+![preview](./assets/preview.png)
+
+👉 [Click here to access !](https://sjbatista.github.io/Game_calendar_design/)
+
+</br>
+
+## 🚀 Technologies an Tools
+
+- HTML
+- CSS
+- JavaScript
+- Git and GitHub
+
+</br>
+
+## 📲 Contact
+
+- salomaojunio@hotmail.com
+- [+55 (31) 9 7533-6222](https://api.whatsapp.com/send?phone=5531975336222&text=Ol%C3%A1%20!%20Encontrei%20seu%20contato%20no%20GitHub%20!)
+- https://www.linkedin.com/in/dev-sjbatista/
+
--- main.js
+++ main.js
@@ -22,27 +22,22 @@ function createCard(date, day, games) {
   `
 }
 
-document.querySelector("#cards").innerHTML = 
-      createCard(
-        "24/11",
-        "quinta",
-        createGame("portugal", "07:00", "serbia") +
-          createGame("brazil", "07:00", "serbia") +
-          createGame("brazil", "07:00", "serbia")
-      ) +
-
-      createCard(
-        "28/11",
-        "segunda",
-        createGame("south_korea", "13:00", "ghana") +
-          createGame("brazil", "13:00", "switzerland") +
-          createGame("portugal", "16:00", "uruguay")
-      ) +
-
-      createCard(
-        "02/12",
-        "sexta",
-        createGame("brazil", "16:00", "cameroon") +
-          createGame("brazil", "16:00", "cameroon") +
-          createGame("portugal", "16:00", "uruguay")
-      )
+document.querySelector("#cards").innerHTML =
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("cameroon", "07:00", "serbia") +
+      createGame("brazil", "13:00", "switzerland")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("serbia", "16:00", "switzerland") +
+      createGame("cameroon", "16:00", "brazil")
+  )
